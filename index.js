0a557a3375f4d2909a5e1c77ef9e7788b7cec28a
Sanket-Munishwar1 <sanketmunishwar7@gmail.com>
2024-01-21 14:10:21 +0530

    update

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-import { config } from "dotenv"
-config()
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const { Configuration , OpenAIApi} = require("openai")
 
-import { Configuration, OpenAIApi } from "openai"
-import readline from "readline"
 
-const openAi = new OpenAIApi(
-  new Configuration({
-    apiKey: process.env.OPEN_AI_API_KEY,
-  })
-)
-
-const userInterface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+const config = new Configuration({
+    apiKey : "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"
 })
 
-userInterface.prompt()
-userInterface.on("line", async input => {
-  const response = await openAi.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: input }],
-  })
-  console.log(response.data.choices[0].message.content)
-  userInterface.prompt()
+const openai = new OpenAIApi(config)
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post("/chat", async(req, res) =>{
+    const { prompt } = req.body
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        max_tokens: 512,
+        temperature: 0,
+        prompt: prompt
+    })
+
+    res.send(completion.data.choices[0].text)
+})
+
+const port =8080
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
 })
